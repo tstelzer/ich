b5ffc85bf5a8bcbee25468a83ea75993bef75c9f
Timm Stelzer <timmstelzer@gmail.com>
2018-04-26 19:56:06 +0200

    Fix documentation typos.

--- source/array.ts
+++ source/array.ts
@@ -1,6 +1,6 @@
 /**
  * Toggle a value in an array.
- * When `xs` contains `a`, append it, otherwise remove _all_ instances.
+ * When `xs` contains `a`, remove _all_ instances, otherwise append it.
  */
 export function toggle(a: any, xs: any[]): any[] {
     if (xs.includes(a)) {
